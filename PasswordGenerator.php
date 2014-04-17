3c9a0c0c8db44df0b5f21a62482b74faebb787c8
Taylor Hornby <havoc@defuse.ca>
2014-04-17 12:59:13 -0600

    Improve tests

--- PasswordGenerator.php
+++ PasswordGenerator.php
@@ -124,5 +124,6 @@ class PasswordGenerator
         }
         return $ints;
     }
+
 }
 ?>
--- test.php
+++ test.php
@@ -1,35 +1,98 @@
 <?php
     require_once('PasswordGenerator.php');
+    echo "Sample Passwords:\n\n";
     $ascii = PasswordGenerator::getASCIIPassword(64);
     $hex = PasswordGenerator::getHexPassword(64);
     $alpha = PasswordGenerator::getAlphaNumericPassword(64);
     $custom = PasswordGenerator::getCustomPassword(array('a', 'b'), 64);
-    echo $ascii, "\n", $hex, "\n", $alpha, "\n", $custom, "\n";
+    echo $ascii, "\n", $hex, "\n", $alpha, "\n", $custom, "\n\n";
 
-    echo "\n\n\n";
+    function failTest($msg)
+    {
+        echo "FAILED: $msg\n";
+        exit(1);
+    }
 
-    echo "ASCII:\n";
-    echo PasswordGenerator::getASCIIPassword(64), "\n";
-    echo PasswordGenerator::getASCIIPassword(64), "\n";
-    echo PasswordGenerator::getASCIIPassword(64), "\n";
-    echo PasswordGenerator::getASCIIPassword(64), "\n";
-    echo PasswordGenerator::getASCIIPassword(64), "\n";
-    echo PasswordGenerator::getASCIIPassword(64), "\n";
-    echo "Alphanumeric:\n";
-    echo PasswordGenerator::getAlphaNumericPassword(64), "\n";
-    echo PasswordGenerator::getAlphaNumericPassword(64), "\n";
-    echo PasswordGenerator::getAlphaNumericPassword(64), "\n";
-    echo PasswordGenerator::getAlphaNumericPassword(64), "\n";
-    echo PasswordGenerator::getAlphaNumericPassword(64), "\n";
-    echo "Hex:\n";
-    echo PasswordGenerator::getHexPassword(64), "\n";
-    echo PasswordGenerator::getHexPassword(64), "\n";
-    echo PasswordGenerator::getHexPassword(64), "\n";
-    echo PasswordGenerator::getHexPassword(64), "\n";
-    echo PasswordGenerator::getHexPassword(64), "\n";
-    echo PasswordGenerator::getHexPassword(64), "\n";
-    echo "Custom (a,b):\n";
-    echo PasswordGenerator::getCustomPassword(array("a","b"), 64), "\n";
-    echo "Improper usage:\n";
-    echo PasswordGenerator::getCustomPassword("abc", 64), "\n";
+    $last = array();
+    $ascii_chars = array();
+    $alpha_chars = array();
+    $hex_chars = array();
+    $custom_chars = array();
+    for ($i = 0; $i < 20; $i++) {
+        $ascii = PasswordGenerator::getASCIIPassword(64);
+        $alpha = PasswordGenerator::getAlphaNumericPassword(64);
+        $hex = PasswordGenerator::getHexPassword(64);
+        $custom = PasswordGenerator::getCustomPassword(array('a', 'b'), 64);
+
+        if (in_array($ascii, $last)) {
+            failTest("Duplicate ASCII password.");
+        }
+
+        if (in_array($alpha, $last)) {
+            failTest("Duplicate AlphaNumeric password.");
+        }
+
+        if (in_array($hex, $last)) {
+            failTest("Duplicate Hex password.");
+        }
+
+        if (in_array($custom, $last)) {
+            failTest("Duplicate Custom password.");
+        }
+
+        if (preg_match("/^[!-~]{64}$/", $ascii) !== 1) {
+            failTest("ASCII regexp failed.");
+        }
+
+        if (preg_match("/^[a-zA-Z0-9]{64}$/", $alpha) !== 1) {
+            failTest("AlphaNumeric regexp failed.");
+        }
+
+        if (preg_match("/^[0-9A-F]{64}$/", $hex) !== 1) {
+            failTest("Hex regexp failed.");
+        }
+
+        if (preg_match("/^[ab]{64}$/", $custom) !== 1) {
+            failTest("Custom regexp failed.");
+        }
+
+        $last[] = $ascii;
+        $last[] = $alpha;
+        $last[] = $hex;
+        $last[] = $custom;
+
+        $ascii_chars = array_merge($ascii_chars, str_split($ascii));
+        $alpha_chars = array_merge($alpha_chars, str_split($alpha));
+        $hex_chars = array_merge($hex_chars, str_split($hex));
+        $custom_chars = array_merge($custom_chars, str_split($custom));
+    }
+
+    $ascii_chars = array_unique($ascii_chars);
+    $alpha_chars = array_unique($alpha_chars);
+    $hex_chars = array_unique($hex_chars);
+    $custom_chars = array_unique($custom_chars);
+
+    if (count($ascii_chars) !== 94) {
+        failTest("Not all ASCII chars are included.");
+    }
+
+    if (count($alpha_chars) !== 62) {
+        failTest("Not all AlphaNumeric chars are included.");
+    }
+
+    if (count($hex_chars) !== 16) {
+        failTest("Not all Hex chars are included.");
+    }
+
+    if (count($custom_chars) !== 2) {
+        failTest("Not all Custom chars are included.");
+    }
+
+    if (PasswordGenerator::getCustomPassword("abc", 64) !== false) {
+        failTest("Improper usage does not return false.");
+    }
+
+    echo "ALL TESTS PASS!\n";
+    exit(0);
+
 ?>
