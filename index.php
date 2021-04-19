29745f3bb365b73734ee625ca0a59fbca4230f33
AlexandroGonSan <alexandrogonsan@outlook.com>
2021-04-19 19:03:10 -0400

    Ready!

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $parameters = [];
+
+    $parameters['title'] = "Generate Passwords:\n\n";
+	require_once('header.php');
+    require_once('PasswordGenerator.php');
+
+    $ascii = PasswordGenerator::getASCIIPassword(64);
+    $hex = PasswordGenerator::getHexPassword(64);
+    $alpha = PasswordGenerator::getAlphaNumericPassword(64);
+    $custom = PasswordGenerator::getCustomPassword(array('a', 'b'), 64);
+    echo $ascii, "\n", $hex, "\n", $alpha, "\n", $custom, "\n";
+
+?>
