d6436e3ea8315828a9dffe5452501004a5b4ec6e
Alexandro Gonçalves Dos Santos <alexandrogonsan@outlook.com>
2021-06-20 00:38:19 +0000

    correction error header duplicate output

--- header.php
+++ header.php
@@ -1,8 +1,3 @@
 <?php
 	require_once('PasswordGenerator.php');
     echo $parameters['title'];
-    $ascii = PasswordGenerator::getASCIIPassword(64);
-    $hex = PasswordGenerator::getHexPassword(64);
-    $alpha = PasswordGenerator::getAlphaNumericPassword(64);
-    $custom = PasswordGenerator::getCustomPassword(array('a', 'b'), 64);
-    echo $ascii, "\n", $hex, "\n", $alpha, "\n", $custom, "\n\n";
