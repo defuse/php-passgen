2f4fcac37c7b8f96ba78147405eca951740e2cbd
Alexandro Gonçalves Dos Santos <alexandrogonsan@outlook.com>
2021-09-20 08:54:10 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet"> 
+<style>
+body {
+    font-family: 'Inconsolata', monospace;
+}
+</style>
 <?php
     if(php_sapi_name() == 'cli') {
         $EOL = "\n";
