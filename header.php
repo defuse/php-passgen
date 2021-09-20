b3b3340070321cb9f78d358a6962c6ae4acf5c76
Alexandro Gonçalves Dos Santos <alexandrogonsan@outlook.com>
2021-09-20 08:57:14 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,4 @@
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-body {
-    font-family: 'Open Sans', sans-serif;
-}
-</style>
+
 <?php
     if(php_sapi_name() == 'cli') {
         $EOL = "\n";
