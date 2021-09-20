b5bce64ba49b19d067a0ab906199f3a54a1a3526
Alexandro Gonçalves Dos Santos <alexandrogonsan@outlook.com>
2021-09-20 08:56:31 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,5 @@
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet"> 
 <style>
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 body {
     font-family: 'Inconsolata', monospace;
 }
